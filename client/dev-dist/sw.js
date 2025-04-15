632c8addfd5b8563acbecdde0e939607ef0af391
sonprogrammer <ods04138@gmail.com>
2025-04-15 21:26:31 +0900

    publish

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.aq0tfqmit6g"
+    "revision": "0.h39hhfirrno"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
