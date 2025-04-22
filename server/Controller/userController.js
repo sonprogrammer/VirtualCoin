f7c3ba337dc54d27411929c4bb7a3747cc3bc203
sonprogrammer <ods04138@gmail.com>
2025-04-22 21:25:46 +0900

    Deploy

--- server/Controller/userController.js
+++ server/Controller/userController.js
@@ -68,15 +68,17 @@ const kakaoLogin = async(req, res) => {
         res.cookie('token', token, {
             httpOnly: true, 
             secure: process.env.NODE_ENV === 'production', 
-            sameSite: 'None',
-            domain: '.virtualcoinn.onrender.com',
+            // sameSite: 'None',
+            sameSite: 'Lax',
+            domain: process.env.NODE_ENV === 'production' ? '.virtualcoinn.onrender.com' : undefined,
             maxAge: 3600000,
         });
         res.cookie('refreshToken', refreshToken, {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production', 
-            sameSite: 'None',
-            domain: '.virtualcoinn.onrender.com',
+            // sameSite: 'None',
+            sameSite: 'Lax',
+            domain: process.env.NODE_ENV === 'production' ? '.virtualcoinn.onrender.com' : undefined,
             maxAge: 7 * 24 * 60 * 60 * 1000, 
         });
         
